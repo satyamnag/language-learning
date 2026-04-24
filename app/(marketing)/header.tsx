f69455cd603bba23c146e07da27d966bd593f9aa
SATYAM NAG <satyamnag642@gmail.com>
2026-04-24 22:14:20 +0530

    logo changed logo.png

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -14,8 +14,8 @@ export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image src="/logo.png" height={150} width={150} alt="Mascot" />
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3 bg-white">
+          <Image src="/logo.png" height={160} width={160} alt="Mascot" />
         </div>
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
