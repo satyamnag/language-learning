a2636e9a96aa13c707144cbed88845137108a88c
SATYAM NAG <satyamnag642@gmail.com>
2026-05-02 22:27:33 +0530

    suno bolo color set

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -19,9 +19,8 @@ export default async function Home() {
 
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
-      {/* Hero image removed */}
       <div className="flex flex-col items-center gap-y-8">
-        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-justify">
           Learn, practice, and master Indian regional languages with{" "}
           <span className="text-blue-600 font-bold">SUNO</span>{" "}
           <span className="text-green-600 font-bold">BOLO</span>.
