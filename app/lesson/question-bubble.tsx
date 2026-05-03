5cdb6b1d949f55ecdbf5fe4576da049409c6436b
SATYAM NAG <satyamnag642@gmail.com>
2026-05-04 04:10:57 +0530

    image removal

--- app/lesson/question-bubble.tsx
+++ app/lesson/question-bubble.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type Props = {
   question: string;
   translation?: string;
@@ -8,20 +6,7 @@ type Props = {
 export const QuestionBubble = ({ question, translation }: Props) => {
   return (
     <div className="flex items-center gap-x-4 mb-6">
-      <Image
-        src="/mascot.svg"
-        alt="Mascot"
-        height={60}
-        width={60}
-        className="hidden lg:block"
-      />
-      <Image
-        src="/mascot.svg"
-        alt="Mascot"
-        height={40}
-        width={40}
-        className="block lg:hidden"
-      />
+      {/* Mascot images removed */}
       <div className="relative py-2 px-4 border-2 rounded-xl text-sm lg:text-base">
         {question}
         {translation && (
