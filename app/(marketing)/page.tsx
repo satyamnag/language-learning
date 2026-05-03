4f79ff6a168f997a471ab8e55b5cf3ad1e25821a
SATYAM NAG <satyamnag642@gmail.com>
2026-05-03 21:59:14 +0530

    updated the homepage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -47,27 +47,29 @@ export default async function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
       <div className="flex flex-col items-center gap-y-8">
-        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-justify">
-          Learn, practice, and master Indian regional languages with{" "}
-          <span className="text-blue-600 font-extrabold">SUNO</span>{" "}
-          <span className="text-green-600 font-extrabold">BOLO</span>.
-        </h1>
         <div className="flex flex-col items-center gap-y-3 max-w-[380px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
           <ClerkLoaded>
             <SignedOut>
-              <SignUpButton mode="modal" fallbackRedirectUrl="/learn">
-                <Button size="lg" variant="primary" className="w-full">
-                  Get Started
-                </Button>
-              </SignUpButton>
-              <SignInButton mode="modal" fallbackRedirectUrl="/learn">
-                <Button size="lg" variant="primaryOutline" className="w-full">
-                  I already have an account
-                </Button>
-              </SignInButton>
+              <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-justify">
+                Learn, practice, and master Indian regional languages with{" "}
+                <span className="text-blue-600 font-extrabold">SUNO</span>{" "}
+                <span className="text-green-600 font-extrabold">BOLO</span>.
+              </h1>
+              <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full mt-4">
+                <SignUpButton mode="modal" fallbackRedirectUrl="/learn">
+                  <Button size="lg" variant="primary" className="w-full">
+                    Get Started
+                  </Button>
+                </SignUpButton>
+                <SignInButton mode="modal" fallbackRedirectUrl="/learn">
+                  <Button size="lg" variant="primaryOutline" className="w-full">
+                    I already have an account
+                  </Button>
+                </SignInButton>
+              </div>
             </SignedOut>
             <SignedIn>
               <div className="flex flex-col sm:flex-row gap-4 w-full">
