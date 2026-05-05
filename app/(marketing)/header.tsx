21861a6bd5cadc53c52a90398087747eef2157b5
SATYAM NAG <satyamnag642@gmail.com>
2026-05-06 02:21:50 +0530

    logged out logo

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -14,7 +14,16 @@ export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        <div className="w-10" />
+        <SignedOut>
+          <Link href="/" className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+            <span className="text-2xl font-extrabold text-blue-600 tracking-wide">
+              bolbolke
+            </span>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <div className="w-10" /> {/* spacer to keep layout balanced */}
+        </SignedIn>
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
