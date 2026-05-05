eb6c1a1d11053fc56c44c06cc774969c9d00ce88
SATYAM NAG <satyamnag642@gmail.com>
2026-05-05 22:51:21 +0530

    signin redirection

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -14,8 +14,7 @@ export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        {/* Removed the bolbolke text/logo – now only in sidebar */}
-        <div className="w-10" /> {/* empty spacer to keep layout balanced */}
+        <div className="w-10" />
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
@@ -24,7 +23,7 @@ export const Header = () => {
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
           <SignedOut>
-            <SignInButton mode="modal" fallbackRedirectUrl="/learn">
+            <SignInButton mode="modal" fallbackRedirectUrl="/">
               <Button size="lg" variant="ghost">Login</Button>
             </SignInButton>
           </SignedOut>
