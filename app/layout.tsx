65d48095215489f20064e8ee9129cb9d8501c194
SATYAM NAG <satyamnag642@gmail.com>
2026-05-06 20:36:41 +0530

    updated browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Language Learning App",
+  title: "Bol Bol Ke",
   description: "Learn Indian Regional languages with Us",
 };
 
