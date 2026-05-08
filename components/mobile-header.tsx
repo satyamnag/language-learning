d5883796c4a1cd8927a94f25d178b0503249d9ec
SATYAM NAG <satyamnag642@gmail.com>
2026-05-08 16:51:16 +0530

    redirect to home page

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
@@ -5,9 +6,11 @@ export const MobileHeader = () => {
     <nav className="lg:hidden fixed top-0 w-full h-[50px] bg-[#7C3AED] z-50 flex items-center border-b border-purple-700 px-4">
       <MobileSidebar />
       <div className="flex-1 flex justify-center">
-        <span className="text-white font-extrabold text-xl tracking-wide">
-          Bol Bol Ke
-        </span>
+        <Link href="/">
+          <span className="text-white font-extrabold text-xl tracking-wide">
+            Bol Bol Ke
+          </span>
+        </Link>
       </div>
       <div className="w-[50px]" /> {/* balanced spacing for symmetry */}
     </nav>
