943ce734a655aaaef74cb2702c30ef23bcd104c7
SATYAM NAG <satyamnag642@gmail.com>
2026-05-08 20:30:20 +0530

    leaderboard page updated

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -7,8 +7,6 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { getAllUsers, getUserProgress, getUserSubscription } from "@/db/queries";
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Promo } from "@/components/promo";
-import { Quests } from "@/components/quests";
 
 const LeaderboardPage = async () => {
   const userProgressData = getUserProgress();
@@ -40,10 +38,6 @@ const LeaderboardPage = async () => {
           points={userProgress.points}
           hasActiveSubscription={isPro}
         />
-        {!isPro && (
-          <Promo />
-        )}
-        <Quests points={userProgress.points} />
       </StickyWrapper>
       <FeedWrapper>
         <div className="w-full flex flex-col items-center">
